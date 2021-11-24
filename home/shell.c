f713ca898976fe8181ad67a2ecfa8b5307f7e154
Monste11osz <monste11osz@gmail.com>
2021-11-24 10:35:11 +0600

    fixed git

--- home/shell.c
+++ home/shell.c
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+
+#define DEVIDED_PID -1
 
 char *getword(char *end)
 {
@@ -73,18 +76,13 @@ void free_line(char **line)
 		free(line[i]);
 	}
 	free(line);
-	/*for(j; line[j]; j++)
-	{
-		free(line[j]);
-	}
-	free(line);*/
 }
 
-void pip_two(char **line, int type)
+
+/*void pip_two(char **line, int type)
 {
 	int fd[2], i;
 	i = type + 1;
-	//line[type] = NULL;
 	pipe(fd);
 	if(fork() == 0)
 	{
@@ -104,19 +102,182 @@ void pip_two(char **line, int type)
 	close(fd[1]);
 	wait(NULL);
 	wait(NULL);
+}*/
+
+char ***recording(char **line, int n)
+{
+	int size = 0, out = 0;
+	int pos = 0;
+	int next_pos = 0;
+	char ***triiple = NULL;
+	triiple = (char***) malloc((n + 1) * sizeof(char**));
+	while(line[size] != NULL)
+	{
+		if(strcmp(line[size], "|") == 0)
+		{
+			triiple[out] = (char**) malloc((size - next_pos + 1) * sizeof(char*));
+			pos = size;
+			for(int size = 0; size < pos - next_pos; size++)
+			{
+				triiple[out][size] = line[size + next_pos];
+			}
+			triiple[out][pos - next_pos] = NULL;
+			next_pos = pos + 1;
+			out++;
+		}
+		size++;
+	}
+	triiple[out] = (char**) malloc((size - next_pos + 1) * sizeof(char*));
+	pos = size;
+	for(int size = 0; size < pos - next_pos; size++)
+	{
+		triiple[out][size] = line[size + next_pos];
+	}
+	triiple[out][pos - next_pos] = NULL;
+	return triiple;
 }
 
-int f_fork(char **line)
+int exec_w(char **line)
 {
-	int fd = 0, index = 0, type = 0;
-	int size = 0;
+	if(execvp(line[0], line) < 0)
+	{
+		perror("exec failed");
+		free_line(line);
+		return 1;
+	}
+	return 0;
+}
+
+/*void pip_mas_N(char **line, int n)
+{
 	char end;
+	int fd[n][2], pid, size, i = 0, type = 0;
+	for(size = 0; size < n + 1; size++)
+	{
+		if(size != n)
+		{
+			pipe(fd[size]);
+		}
+		if((pid = fork()) == 0)
+		{
+			if(size != 0)
+			{
+				dup2(fd[size - 1][0], 0);
+				close(fd[size - 1][0]);
+				close(fd[size - 1][1]);
+			}
+			if(size != n)
+			{
+				dup2(fd[size][1], 1);
+				close(fd[size][0]);
+				close(fd[size][1]);
+			}
+			exec_w(line);
+		}
+		else
+		{
+			if(size != 0)
+			{
+				close(fd[size - 1][1]);
+				close(fd[size - 1][0]);
+			}
+		}
+		while(line[i] != NULL)
+		{
+			i++;
+		}
+		i++;
+		type = i;
+	}
+	close(fd[size][1]);
+	close(fd[size][0]);
+	for(int j = 0; j < n + 1; j++)
+	{
+		wait(NULL);
+	}
+}*/
+
+void pipe_N(char **line, int n)
+{
+	int size;
+	int fd[n][2], pid;
+	char ***triple = recording(line, n);
+	for(size = 0; size < n + 1; size++)
+	{
+		if(size != n)
+		{
+			pipe(fd[size]);
+		}
+		if((pid = fork()) == 0)
+		{
+			if(size != 0)
+			{
+				dup2(fd[size - 1][0], 0);
+				close(fd[size - 1][0]);
+				close(fd[size - 1][1]);
+			}
+			if(size != n)
+			{
+				dup2(fd[size][1], 1);
+				close(fd[size][0]);
+				close(fd[size][1]);
+			}
+
+			if(execvp(triple[size][0], triple[size]) < 0);
+			{
+				perror("exec failed");
+				exit(1);
+			}
+		}
+		else
+		{
+			if(size != 0)
+			{
+				close(fd[size - 1][1]);
+				close(fd[size - 1][0]);
+			}
+		}
+	}
+	close(fd[size][1]);
+	close(fd[size][0]);
+	for(int i = 0; i < n + 1; i++)
+	{
+		wait(NULL);
+	}
+}
+
+int f_fork(char **line, int *pid_in_phone, int fon_c)
+{
+	int fd = 0, index = 0, type = 0, pid;
+	int size = 0, n = 0, fon = 0;
+	char end;
+	char *getenv(const char *user);
+	int setenv(const char *user, const char *value, int overwrite);
 	while(line[size] != NULL)
         {
-                if(strcmp(line[size], ">") == 0)
+		if(strcmp(line[size], "cd") == 0)
+		{
+			if(line[size + 1] == NULL || strcmp(line[size + 1], "~") == 0)
+			{
+				chdir(getenv("home"));
+				return 0;
+			}
+			else
+			{
+				chdir(line[size + 1]);
+				return 0;
+			}
+		}
+		else if(strcmp(line[size], "&") == 0)
+		{
+			fon = 1;
+			free(line[size]);
+			line[size] = NULL;
+			break;
+		}
+                else if(strcmp(line[size], ">") == 0)
                 {
 			free(line[size]);
-			line[size] = getword(&end);
                         fd = open(line[size + 1], O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
                         if(fd < 0)
                         {
@@ -131,8 +292,7 @@ int f_fork(char **line)
                	else if(strcmp(line[size], "<") == 0)
                	{
 			free(line[size]);
-			line[size] = getword(&end);
-                       	fd = open(line[size + 1], O_RDONLY|O_TRUNC);
+                       	fd = open(line[size + 1], O_RDONLY, S_IRUSR|S_IWUSR);
                        	if(fd < 0)
                        	{
                                	perror("Open failed");
@@ -145,33 +305,45 @@ int f_fork(char **line)
                 }
                 else if(strcmp(line[size], "|") == 0)
                 {
+			n++;
                         type = size;
-			free(line[size]);
-			line[size] = getword(&end);
-                        line[size] = NULL;
-                	break;
+			//free(line[size]);
+			//line[size] = getword(&end);
+                        //line[size] = NULL;
+                	//break;
                 }
         	size++;
         }
 	size = 0;
 	if(type != 0)
 	{
-		pip_two(line, type);
+		//pip_two(line, type);
+		pipe_N(line, n);
+		//pip_mas_N(line, n);
 	}
 	else
 	{
-		if(fork() == 0)
+		if(pid = fork() != 0)
+		{
+			if(fon == 0)
+			{
+				wait(NULL);
+			}
+			else
+			{
+				pid_in_phone = realloc(pid_in_phone, fon * sizeof(int));
+				pid_in_phone[fon_c] = pid;
+				fon_c++;
+
+			}
+		}
+		else
 		{
 			if(fd > 0)
 			{
 				dup2(fd, index);
 			}
-			if(execvp(line[0], line) < 0)
-			{
-				perror("exec failed");
-				free_line(line);
-				return 1;
-			}
+			exec_w(line);
 		}
 	}
 	if(fd > 0)
@@ -179,20 +351,38 @@ int f_fork(char **line)
 		close(fd);
 	}
 	fd = 0;
+	free(pid_in_phone);
 	return 0;
+}
+
+void handler(int signo)
+{
+	int pid = DEVIDED_PID;
+	puts("\nreseived SIGINT");
+	if(pid != DEVIDED_PID)
+	{
+		kill(pid, SIGINT);
+		pid = DEVIDED_PID;
+	}
+	//exit(1);
 }
 
 int main()
 {
+	int *pid_in_phone = NULL, fon_c = 0;
 	char **line = getlist();
+	signal(SIGINT, handler);
 	while(strcmp(line[0], "exit") != 0 && strcmp(line[0], "quit") != 0)
 	{
-		f_fork(line);
+		f_fork(line, pid_in_phone, fon_c);
 		free_line(line);
 		wait(NULL);
 		line = getlist();
 	}
 	free_line(line);
+	for(int i = 0; i < fon_c; i++)
+	{
+		waitpid(pid_in_phone[i], NULL, 0);
+	}
 	return 0;
 }
-
